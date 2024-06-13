6aa041dc43bfb59ee183c51fc531047723953267
Encapsulateed <alexey.mitroshkin@gmail.com>
2024-06-13 19:12:31 +0300

    lab1

--- lab2/main.py
+++ lab2/main.py
@@ -3,19 +3,18 @@ from llvmlite import ir
 # Создание модуля
 module = ir.Module(name="simple_module")
 
-# Объявление функции main
+# Указание целевого тройного и макета данных
+module.triple = "none"
+module.data_layout = "none"
+
 func_type = ir.FunctionType(ir.IntType(32), [])
 main_func = ir.Function(module, func_type, name="main")
 
-# Создание блока входа для функции main
 block = main_func.append_basic_block(name="entry")
 builder = ir.IRBuilder(block)
 
-# Генерация инструкции для сложения 353 и 48
-result = builder.add(ir.Constant(ir.IntType(32), 353), ir.Constant(ir.IntType(32), 48))
+result = builder.add(ir.Constant(ir.IntType(32), 353), ir.Constant(ir.IntType(32), 48),name="var")
 
-# Возвращение результата
 builder.ret(result)
 
-# Печать сгенерированного LLVM кода
 print(module)
